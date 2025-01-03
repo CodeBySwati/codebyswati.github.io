239cbaef27ee8fb8fe7568ca4d9c9e98bb9ada2c
Swati Srivastava <101457575+CodeBySwati@users.noreply.github.com>
2025-01-03 16:48:25 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'includes/header.php';
-require_once 'includes/hero.php';
-require_once 'includes/about.php';
-require_once 'includes/skills.php';
-require_once 'includes/projects.php';
-require_once 'includes/contact.php';
-require_once 'includes/footer.php';
-?>
